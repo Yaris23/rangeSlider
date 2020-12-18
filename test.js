9cc0fdb76cc250aeded6c6f48825e9766cf26d97
unknown <yaris23-rabota@yandex.ru>
2020-12-18 18:25:04 +0300

    create rangeSlider function

--- test.js
+++ test.js
@@ -1,5 +1,32 @@
-import {RangeLine} from './RangeLine.js';
-import {Pointer} from './Pointer.js'
 
-let rangeLine = new RangeLine(0, 12);
-rangeLine.show()
+
+
+function createRangeSlider(startDivClass){
+    let params = {
+        'minRange' : 0,
+        'maxRange' : 10,
+    }
+
+    let container = document.querySelector(startDivClass);
+
+    let myRangeSlider = document.createElement('div');
+    let rangeLine = document.createElement('div');
+    let pointer = document.createElement('div');
+    let sliderParams = document.createElement('div');
+
+    myRangeSlider.classList.add('myRangeSlider');
+    rangeLine.classList.add('rangeLine');
+    pointer.classList.add('pointer');
+    sliderParams.classList.add('sliderParams')
+
+    container.append(myRangeSlider);
+    myRangeSlider.append(sliderParams);
+    myRangeSlider.append(rangeLine);
+    rangeLine.append(pointer);
+
+    console.log(params.minRange)
+}
+
+createRangeSlider('.container');
+createRangeSlider('.container');
+createRangeSlider('.container');
