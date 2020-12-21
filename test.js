ed333adc88a08156b2a5f7069dbc30e935a0f69d
unknown <yaris23-rabota@yandex.ru>
2020-12-21 14:46:32 +0300

    add gitignore, add jquery

--- test.js
+++ test.js
@@ -1,32 +1,50 @@
+$.fn.myRangeSlider = function (options) {
+    let opt = options;
 
-
-
-function createRangeSlider(startDivClass){
-    let params = {
-        'minRange' : 0,
-        'maxRange' : 10,
-    }
-
-    let container = document.querySelector(startDivClass);
-
-    let myRangeSlider = document.createElement('div');
-    let rangeLine = document.createElement('div');
-    let pointer = document.createElement('div');
-    let sliderParams = document.createElement('div');
-
-    myRangeSlider.classList.add('myRangeSlider');
-    rangeLine.classList.add('rangeLine');
-    pointer.classList.add('pointer');
-    sliderParams.classList.add('sliderParams')
-
-    container.append(myRangeSlider);
-    myRangeSlider.append(sliderParams);
-    myRangeSlider.append(rangeLine);
-    rangeLine.append(pointer);
-
-    console.log(params.minRange)
+    console.log(opt.left, opt.right);
 }
 
-createRangeSlider('.container');
-createRangeSlider('.container');
-createRangeSlider('.container');
+$().myRangeSlider({
+    left : 0,
+    right : 10
+});
+
+
+// function createRangeSlider(startDivClass){
+//     let params = {
+//         'minRange' : 0,
+//         'maxRange' : 10,
+//     }
+
+//     let container = document.querySelector(startDivClass);
+
+//     let myRangeSlider = document.createElement('div');
+//     let rangeLine = document.createElement('div');
+//     let pointer = document.createElement('div');
+//     let sliderParams = document.createElement('div');
+//     let inputMinRange = document.createElement('input');
+//     let inputMaxRange = document.createElement('input');
+
+//     myRangeSlider.classList.add('myRangeSlider');
+//     rangeLine.classList.add('rangeLine');
+//     pointer.classList.add('pointer');
+//     sliderParams.classList.add('sliderParams');
+//     inputMinRange.classList.add('inputMinRange')
+//     inputMaxRange.classList.add('inputMaxRange')
+
+//     container.append(myRangeSlider);
+//     myRangeSlider.append(sliderParams);
+//     myRangeSlider.append(rangeLine);
+//     rangeLine.append(pointer);
+//     sliderParams.append(inputMinRange);
+//     sliderParams.append(inputMaxRange);
+
+
+//     inputMinRange.value = params.minRange;
+//     inputMaxRange.value = params.maxRange;
+
+
+//     console.log(params.minRange)
+// }
+
+// createRangeSlider('.container');
