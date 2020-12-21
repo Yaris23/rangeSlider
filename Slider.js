af1bb9884c577e0a98f60a71412acafecb4ed9f2
Yaris23 <Yaris23-rabota@yandex.ru>
2020-12-21 20:37:30 +0300

    123

--- Slider.js
+++ Slider.js
@@ -5,14 +5,20 @@ import {Pointer} from './Pointer.js';
 export class Slider{
     container;
     slider;
-    line = new Line();
-    pointer = new Pointer();
+    line;
+    pointer;
+    options;
 
     constructor(container){
+        this.options = document.createElement('div');
+        this.options.classList.add('options')
+        this.line = new Line();
+        this.pointer = new Pointer();
         this.container = document.querySelector(container);
         this.slider = document.createElement('div');
         this.slider.classList.add('slider');
         this.container.append(this.slider);
+        this.slider.append(this.options);
         this.slider.append(this.line.element);
         this.line.element.append(this.pointer.element);
     }
